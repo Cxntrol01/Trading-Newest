d337e40324ab7a4e20fd3650e52b454589a0e600
Cxntrol01 <102139461+Cxntrol01@users.noreply.github.com>
2026-04-08 18:49:39 +0100

    Update ChartContainer.tsx

--- components/ChartContainer.tsx
+++ components/ChartContainer.tsx
@@ -1,25 +1,17 @@
-"use client";
-
 import ChartHeader from "./ChartHeader";
-import ChartFooter from "./ChartFooter";
 import FullscreenChart from "./FullscreenChart";
+import ChartSettings from "./ChartSettings";
 
-export default function ChartContainer({
-  symbol,
-  children,
-}: {
-  symbol: string;
-  children: React.ReactNode;
-}) {
+export default function ChartContainer({ children, symbol }: { children: React.ReactNode; symbol: string }) {
   return (
-    <div className="bg-gray-950 p-3 rounded-lg border border-gray-800">
+    <div className="chart-container">
       <ChartHeader symbol={symbol} />
 
       <FullscreenChart>
         {children}
       </FullscreenChart>
 
-      <ChartFooter />
+      <ChartSettings onChange={(setting) => console.log("Setting changed:", setting)} />
     </div>
   );
 }
