ffc3b714fd00a5f8d9d92d65eb2b868a45e8076a
Cxntrol01 <102139461+Cxntrol01@users.noreply.github.com>
2026-04-11 15:15:42 +0100

    Create PriceChart.tsx

---
+++ components/PriceChart.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function PriceChart() {
+  return (
+    <div className="w-full h-full bg-black border border-gray-800 rounded-lg overflow-hidden flex items-center justify-center">
+      <p className="text-gray-500">Chart canvas</p>
+    </div>
+  );
+}
