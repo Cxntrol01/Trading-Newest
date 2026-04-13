97efc7604d26ca2600326a716f1235672aabee6d
Cxntrol01 <102139461+Cxntrol01@users.noreply.github.com>
2026-04-13 20:01:22 +0100

    Update page.tsx

--- app/workspace/page.tsx
+++ app/workspace/page.tsx
@@ -1,11 +1,22 @@
 "use client";
 
-import ChartWorkspace from "@/components/ChartWorkspace";
+import ChartGrid from "@/components/ChartGrid";
+import OrderBook from "@/components/OrderBook";
 
 export default function WorkspacePage() {
   return (
-    <div className="w-full h-screen bg-black">
-      <ChartWorkspace />
+    <div className="w-full h-screen flex flex-col bg-black text-white">
+
+      {/* MULTICHART */}
+      <div className="flex-1 min-h-[300px] overflow-hidden">
+        <ChartGrid />
+      </div>
+
+      {/* ORDERBOOK BELOW MULTICHART */}
+      <div className="h-[260px] overflow-y-auto border-t border-gray-800 shrink-0">
+        <OrderBook symbol="AAPL" />
+      </div>
+
     </div>
   );
 }
